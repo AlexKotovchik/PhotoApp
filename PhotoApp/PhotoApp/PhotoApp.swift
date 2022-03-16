233cb28336c07a5e4232be994b060867cf8b47e5
AlexKotov <alexkotov0408@gmail.com>
2022-03-17 01:39:28 +0300

    added project

---
+++ PhotoApp/PhotoApp/PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by AlexKotov on 17.03.22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
